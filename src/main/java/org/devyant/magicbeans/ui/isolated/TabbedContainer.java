43b6f992ee30f63197da1eb7bff0347daa2cf6f3
Filipe Tavares <fptavares@gmail.com>
2006-03-26 19:20:07 +0000

    *** empty log message ***

---
+++ src/main/java/org/devyant/magicbeans/ui/isolated/TabbedContainer.java
@@ -0,0 +1,43 @@
+/*
+ * Magic Beans: a library for GUI generation and component-bean mapping.
+ * Copyright (C) 2005  Filipe Tavares
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * devYant, devyant@devyant.org
+ * Rua Simao Bolivar 203 6C, 4470-214 Maia, Portugal.
+ *
+ */
+package org.devyant.magicbeans.ui.isolated;
+
+
+/**
+ * TabbedContainer is a <b>cool</b> class.
+ * 
+ * @author ftavares
+ * @version $Revision$ $Date$ ($Author$)
+ * @since Nov 26, 2005 4:44:43 PM
+ */
+public interface TabbedContainer {
+    /**
+     * @return The main panel
+     */
+    public Object getMainPanel();
+    /**
+     * @param title
+     * @param component
+     */
+    public void addSecondaryPanel(final String title, final Object component);
+}
